807a0f804112b92df8413686002d99aea20388bb
Montoya Aguilar <emont@csu.fullerton.edu>
2026-02-11 23:10:47 -0800

    Updated Discord Button

--- src/data/socials.js
+++ src/data/socials.js
@@ -2,5 +2,5 @@ export const socials = {
   github: "https://github.com/eemont",
   linkedin: "https://www.linkedin.com/in/eemont",
   email: "emont.business@gmail.com",
-  discord: "http://discordapp.com/users/266374551451992064",
+  discord: "https://discord.gg/8aggrxZQ",
 };
